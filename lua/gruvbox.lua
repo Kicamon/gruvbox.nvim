23d62bf4fe1119ac36897e0dc27aff2dd4cd7d28
KicamonIce <1532931259@qq.com>
2023-09-27 18:36:06 +0800

    2023/09/27,18:36

--- lua/gruvbox.lua
+++ lua/gruvbox.lua
@@ -28,7 +28,7 @@ local colors = {
 
 local M = {}
 
-M.setup = function()
+M.setcolor = function()
   local config = require("gruvbox").config
 
   vim.g.terminal_color_0 = colors.bg0
@@ -992,6 +992,62 @@ M.setup = function()
   end
 
   return groups
+end
+
+M.config = {
+  undercurl            = true,
+  underline            = true,
+  bold                 = true,
+  italic               = {
+    strings   = true,
+    comments  = true,
+    operators = false,
+    folds     = true,
+  },
+  strikethrough        = true,
+  invert_selection     = false,
+  invert_signs         = false,
+  invert_tabline       = false,
+  invert_intend_guides = false,
+  inverse              = true, -- invert background for search, diffs, statuslines and errors
+  contrast             = "",   -- can be "hard", "soft" or empty string
+  palette_overrides    = {},
+  overrides            = {},
+  dim_inactive         = false,
+  transparent_mode     = false,
+}
+
+function M.setup(config)
+  if config ~= nil and type(config.italic) == "boolean" then
+    vim.notify(
+      "[gruvbox] italic config has change. please check https://github.com/ellisonleao/gruvbox.nvim/issues/220",
+      vim.log.levels.WARN
+    )
+    config.italic = M.config.italic
+  end
+  M.config = vim.tbl_deep_extend("force", M.config, config or {})
+end
+
+M.load = function()
+  if vim.version().minor < 8 then
+    vim.notify_once("gruvbox.nvim: you must use neovim 0.8 or higher")
+    return
+  end
+
+  -- reset colors
+  if vim.g.colors_name then
+    vim.cmd.hi("clear")
+  end
+
+  vim.g.colors_name = "gruvbox"
+  vim.o.termguicolors = true
+
+  local groups = M.setcolor()
+
+  -- add highlights
+  for group, settings in pairs(groups) do
+    vim.api.nvim_set_hl(0, group, settings)
+  end
 end
 
 return M
