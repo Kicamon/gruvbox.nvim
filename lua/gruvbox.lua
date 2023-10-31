fd8fcaa95cb964e0ef014dc5fa7b6fc4a15b3e65
KicamonIce <1532931259@qq.com>
2023-11-01 00:02:42 +0800

    2023/11/01,00:02

--- lua/gruvbox.lua
+++ lua/gruvbox.lua
@@ -885,7 +885,7 @@ M.setcolor = function()
     PmenuSel = { bg = "#282C34", fg = "NONE" },
     -- Pmenu = { fg = "#C5CDD9", bg = "#22252A" },
     Pmenu = { fg = colors.fg0, bg = "#22252A" },
-    PmenuSec = { link = "FloatWin" },
+    PmenuSec = { fg = colors.fg0, bg = "NONE" },
 
     CmpItemAbbrDeprecated = { fg = "#7E8294", bg = "NONE", strikethrough = true },
     CmpItemAbbrMatch = { fg = "#82AAFF", bg = "NONE", bold = true },
