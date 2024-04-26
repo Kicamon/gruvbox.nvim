d418c45c8f1225af3a5b2c7b750f9ec5e2ae133d
KicamonIce <1532931259@qq.com>
2024-04-26 23:44:48 +0800

    2024/04/26,23:44

--- lua/gruvbox.lua
+++ lua/gruvbox.lua
@@ -954,7 +954,7 @@ M.setcolor = function()
     CmpItemKindColor = { fg = "#D8EEEB", bg = "#58B5A8" },
     CmpItemKindTypeParameter = { fg = "#D8EEEB", bg = "#58B5A8" },
 
-    MiniIndentscopeSymbol = { fg = '#b8bb26', bg = "NONE" },
+    MiniIndentscopeSymbol = { fg = '#98971a', bg = "NONE" },
   }
 
   for group, hl in pairs(config.overrides) do
